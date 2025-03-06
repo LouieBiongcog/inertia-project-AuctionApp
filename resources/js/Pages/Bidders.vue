fd3efa7780a32774e5e82fa4cba1c0ca68ae3575
LouieBiongcog <louieseoul09@gmail.com>
2025-03-06 19:56:00 +0800

    final

--- resources/js/Pages/Bidders.vue
+++ resources/js/Pages/Bidders.vue
@@ -1,14 +1,25 @@
 <script setup>
-    import Navbar from "../Components/navbar.vue";
-    import { ref, watchEffect } from "vue";
-    import { router } from "@inertiajs/vue3";
+import Navbar from "../Components/navbar.vue";
+import { ref, computed } from "vue";
+import { router } from "@inertiajs/vue3";
 
-    const props = defineProps({ bidders: Array, search: String });
-    const searchQuery = ref(props.search || "");
+const props = defineProps({ bidders: Array, search: String });
+const searchQuery = ref(props.search || "");
 
-    const searchBidders = () => {
-        router.get("/bidders", { search: searchQuery.value }, { preserveState: true });
-    };
+
+const filteredBidders = computed(() => {
+    if (!searchQuery.value) return props.bidders;
+    return props.bidders.filter(bidder =>
+        `${bidder.first_name} ${bidder.last_name} ${bidder.email}`
+            .toLowerCase()
+            .includes(searchQuery.value.toLowerCase())
+    );
+});
+
+
+const searchBidders = () => {
+    router.get("/bidders", { search: searchQuery.value }, { preserveState: true });
+};
 </script>
 
 <template>
@@ -19,29 +30,28 @@
         <input
             type="text"
             v-model="searchQuery"
-            @input="searchBidders"
             placeholder="Search bidders..."
             class="p-2 border rounded-md w-full mb-4"
         />
 
         <table class="bg-white p-4 shadow-md rounded-lg w-full border-collapse">
-    <thead>
-        <tr class="border-b">
-            <th class="py-2 px-4 text-left">First Name</th>
-            <th class="py-2 px-4 text-left">Last Name</th>
-            <th class="py-2 px-4 text-left">Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="bidder in bidders" :key="bidder.id" class="border-b">
-            <td class="py-2 px-4">{{ bidder.first_name }}</td>
-            <td class="py-2 px-4">{{ bidder.last_name }}</td>
-            <td class="py-2 px-4">{{ bidder.email }}</td>
-        </tr>
-    </tbody>
-</table>
-
+            <thead>
+                <tr class="border-b">
+                    <th class="py-2 px-4 text-left">First Name</th>
+                    <th class="py-2 px-4 text-left">Last Name</th>
+                    <th class="py-2 px-4 text-left">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="bidder in filteredBidders" :key="bidder.id" class="border-b">
+                    <td class="py-2 px-4">{{ bidder.first_name }}</td>
+                    <td class="py-2 px-4">{{ bidder.last_name }}</td>
+                    <td class="py-2 px-4">{{ bidder.email }}</td>
+                </tr>
+                <tr v-if="filteredBidders.length === 0">
+                    <td colspan="3" class="py-2 px-4 text-center text-gray-500">No bidders found</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
-
-
--- resources/js/Pages/Cars.vue
+++ resources/js/Pages/Cars.vue
@@ -1,3 +1,27 @@
+
+<script setup>
+import Navbar from "../Components/navbar.vue";
+import { ref, computed } from "vue";
+import { router } from "@inertiajs/vue3";
+
+const props = defineProps({ cars: Array, search: String });
+const searchQuery = ref(props.search || "");
+
+
+const filteredCars = computed(() => {
+    if (!searchQuery.value) return props.cars;
+    return props.cars.filter(car =>
+        `${car.make} ${car.model} ${car.year} ${car.miles}`
+            .toLowerCase()
+            .includes(searchQuery.value.toLowerCase())
+    );
+});
+
+
+const searchCars = () => {
+    router.get("/cars", { search: searchQuery.value }, { preserveState: true });
+};
+</script>
 <template>
     <Navbar />
     <div class="p-4">
@@ -6,42 +30,32 @@
         <input
             type="text"
             v-model="searchQuery"
-            @input="searchCars"
             placeholder="Search cars..."
             class="p-2 border rounded-md w-full mb-4"
         />
 
         <table class="bg-white p-4 shadow-md rounded-lg w-full border-collapse">
-    <thead>
-        <tr class="border-b">
-            <th class="py-2 px-4 text-left">Make</th>
-            <th class="py-2 px-4 text-left">Model</th>
-            <th class="py-2 px-4 text-left">Year</th>
-            <th class="py-2 px-4 text-left">Miles</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="car in cars" :key="car.id" class="border-b">
-            <td class="py-2 px-4">{{ car.make }}</td>
-            <td class="py-2 px-4">{{ car.model }}</td>
-            <td class="py-2 px-4">{{ car.year }}</td>
-            <td class="py-2 px-4">{{ car.miles }} miles</td>
-        </tr>
-    </tbody>
-</table>
-
+            <thead>
+                <tr class="border-b">
+                    <th class="py-2 px-4 text-left">Make</th>
+                    <th class="py-2 px-4 text-left">Model</th>
+                    <th class="py-2 px-4 text-left">Year</th>
+                    <th class="py-2 px-4 text-left">Miles</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="car in filteredCars" :key="car.id" class="border-b">
+                    <td class="py-2 px-4">{{ car.make }}</td>
+                    <td class="py-2 px-4">{{ car.model }}</td>
+                    <td class="py-2 px-4">{{ car.year }}</td>
+                    <td class="py-2 px-4">{{ car.miles }} miles</td>
+                </tr>
+                <tr v-if="filteredCars.length === 0">
+                    <td colspan="4" class="py-2 px-4 text-center text-gray-500">No cars found</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
-<script setup>
-import Navbar from "../Components/navbar.vue";
-import { ref } from "vue";
-import { router } from "@inertiajs/vue3";
 
-const props = defineProps({ cars: Array, search: String });
-const searchQuery = ref(props.search || "");
-
-const searchCars = () => {
-    router.get("/cars", { search: searchQuery.value }, { preserveState: true });
-};
-</script>
